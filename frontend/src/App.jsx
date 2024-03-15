aa1cd451f4fee64c21ea461997cda4dd22301af8
Yusuf Quddus <yusufquddus@berkeley.edu>
2024-03-14 23:07:07 -0700

    setup usestate variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react'
 import Character from './components/Character'
 
 const App = ({characters}) => {
+  const [chars, addCharacters] = useState(characters)
+  const [name, setName] = useState('')
+  const [level, setLevel] = useState('')
+  const [subclass, addSubclass] = useState([])
+  const [race, setRace] = useState('')
+  const [campaign, setCampaign] = useState('')
+  const [dead, establishLife] = useState('')
+  const [story, setStory] = useState('')
+  const [status, setStatus] = useState('')
+  const [image, setImage] = useState('')
+
+
+  const addChar = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+
   console.log('App works!')
   return (
     <div>
       <h1>Characters</h1>
+      <form onSubmit={addChar}>
+        <input />
+        <button type="submit">submit</button>
+      </form>
       <ul>{characters.map(c => <Character key={c.id} character={c}/>)}</ul>
     </div>
   )
