2df933e701e1c4f8f1f25084545582d1aad8cda9
Yusuf Quddus <yusufquddus@berkeley.edu>
2024-03-27 21:47:48 -0700

    tokens error handling

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -19,8 +19,12 @@ const errorHandler = (error, req, res, next) => {
         return res.status(400).send({error: 'malformed id'})
     } else if (error.name === 'ValidationError') {
         return res.status(400).send({error: error.message})
-    }else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
         return res.status(400).json({ error: 'expected `username` to be unique' })
+    } else if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({ error: 'invalid token' })
+    } else if (error.name === 'TokenExpiredError') {
+        return res.status(401).json({ error: 'expired token' })
     }
 
     next(error)
